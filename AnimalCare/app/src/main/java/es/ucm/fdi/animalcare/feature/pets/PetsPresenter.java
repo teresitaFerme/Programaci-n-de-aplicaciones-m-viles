0ad6d41d02a3c4a90b1968f3d7d7ef895456315d
Eva Verdú Rodríguez <everdu@ucm.es>
2021-04-30 17:15:55 +0200

    pets

--- AnimalCare/app/src/main/java/es/ucm/fdi/animalcare/feature/pets/PetsPresenter.java
+++ AnimalCare/app/src/main/java/es/ucm/fdi/animalcare/feature/pets/PetsPresenter.java
@@ -1,15 +1,20 @@
 package es.ucm.fdi.animalcare.feature.pets;
 
+import android.content.Context;
+
+import java.util.List;
+
 import es.ucm.fdi.animalcare.R;
 import es.ucm.fdi.animalcare.base.BasePresenter;
+import es.ucm.fdi.animalcare.data.Pets;
 
 public class PetsPresenter extends BasePresenter {
     private PetsView mPetsView;
     private PetsModel mPetsModel;
 
     PetsPresenter(PetsView petsView){
-        mPetsModel = new PetsModel();
-        mPetsView = petsView;
+        this.mPetsView = petsView;
+        mPetsModel = new PetsModel((Context) petsView);
     }
 
     public void validateNewPet(String name, String type){
@@ -17,10 +22,18 @@ public class PetsPresenter extends BasePresenter {
         else{
             mPetsModel.saveNewPet(name,type);
             mPetsView.NewPetSuccessful();
+
         }
     }
 
     public void addNewPet() {
-            mPetsView.addNewPet();
+        mPetsView.addNewPet();
+    }
+
+    public List<Pets> validateUserPets(String s) {
+        if(s == null){
+            mPetsView.noRegister();
+        }
+        return mPetsModel.getPets(Integer.parseInt(s));
     }
 }
