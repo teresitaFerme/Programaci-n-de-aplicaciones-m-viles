0fe410d36e83974dcfd6cdea734ec1a15db52c23
hpascua <43760125+hpascua@users.noreply.github.com>
2021-05-15 21:11:16 +0200

    resolve conflicts

--- AnimalCare/app/src/main/java/es/ucm/fdi/animalcare/feature/calendar/CalendarActivity.java
+++ AnimalCare/app/src/main/java/es/ucm/fdi/animalcare/feature/calendar/CalendarActivity.java
@@ -6,9 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.loader.app.LoaderManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 import es.ucm.fdi.animalcare.R;
 import es.ucm.fdi.animalcare.base.BaseActivity;
-import es.ucm.fdi.animalcare.base.BasePresenter;
+import es.ucm.fdi.animalcare.data.Task;
+import es.ucm.fdi.animalcare.data.User;
 import es.ucm.fdi.animalcare.feature.pets.PetsActivity;
 import es.ucm.fdi.animalcare.feature.settings.SettingsActivity;
 import es.ucm.fdi.animalcare.feature.toolbar.ToolBarManagement;
@@ -16,7 +30,16 @@ import es.ucm.fdi.animalcare.feature.upcoming.UpcomingActivity;
 import es.ucm.fdi.animalcare.feature.user.UserActivity;
 
 public class CalendarActivity extends BaseActivity implements CalendarView, ToolBarManagement {
+    private int TASK_LOADER_ID=50;
+    private User user;
     private CalendarPresenter mCalendarPresenter;
+    private List<EventDay> events = new ArrayList<>();
+    private Calendar mCalendar;
+    private com.applandeo.materialcalendarview.CalendarView mCalendarView;
+    private RecyclerView mRecyclerView;
+    private TaskLoaderCallbacks mTaskLoaderCallbacks = new TaskLoaderCallbacks(this, this);
+    private TasksListAdapter mAdapter;
+    private List<Task> mTasks= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +54,50 @@ public class CalendarActivity extends BaseActivity implements CalendarView, Tool
 
 
         mCalendarPresenter = new CalendarPresenter(this);
+
+        mCalendar = Calendar.getInstance();
+
+        mCalendarView = findViewById(R.id.calendarView);
+        mCalendarPresenter.prepareEvents(user.getmId());
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mAdapter = new TasksListAdapter(this, mTasks);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        LoaderManager loaderManager = LoaderManager.getInstance(this);
+        if(loaderManager.getLoader(TASK_LOADER_ID) != null){
+            loaderManager.initLoader(TASK_LOADER_ID,null, mTaskLoaderCallbacks);
+        }
+
+        mCalendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Calendar calendar = eventDay.getCalendar();
+                searchEventsOfDay(calendar);
+            }
+        });
+    }
+
+    @Override
+    public void setEvents(List<EventDay> events){
+        mCalendarView.setEvents(events);
+    }
+
+    public void searchEventsOfDay(Calendar calendar){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(TaskLoaderCallbacks.EXTRA_DATETIMESTRING, dateFormat.format(calendar.getTime()));
+        queryBundle.putInt(TaskLoaderCallbacks.EXTRA_USERID, user.getmId());
+        LoaderManager.getInstance(this)
+                .restartLoader(TASK_LOADER_ID, queryBundle, mTaskLoaderCallbacks);
+    }
+
+    public void updateTasksList(List<Task> data){
+        mAdapter.setTasksData(data);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
